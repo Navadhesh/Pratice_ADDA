68b672f80c534ae948b4732b1722513847b04005
Navadhesh <navadheshreddy30@gmail.com>
2024-04-14 17:31:58 +0530

    Added files

---
+++ JavaCode1.java
@@ -0,0 +1,9 @@
+public class JavaCode1
+{
+    public static void main(String[] args) {
+        System.out.println("Name: Navadhesh.M");
+        System.out.println("Reg.no: 21MIC0024");
+        System.out.println("Age:21 years");
+        System.out.println("3rd year VITIAN");
+    }
+}
